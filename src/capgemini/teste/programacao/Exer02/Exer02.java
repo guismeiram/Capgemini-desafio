e923bf809076ff0903377d4ff205ffdd238a8ee3
Guilherme Mendes <boralagui@outlook.com>
2022-02-18 06:37:16 -0300

    Quarto commit

--- src/capgemini/teste/programacao/Exer02/Exer02.java
+++ src/capgemini/teste/programacao/Exer02/Exer02.java
@@ -7,32 +7,59 @@ import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
 
 public class Exer02 {
-    public static boolean validaSenha(String password)
-    {
-    		String regex = "^(?=.*[0-9])"
-                    + "(?=.*[a-z])(?=.*[A-Z])"
-                    + "(?=.*[@#$%^&+=])"
-                    + "(?=\\S+$).{8,20}$";
-
-    		Pattern p = Pattern.compile(regex);
-
-    		if (password == null) {
-    			return false;
-    		}
-
-    		Matcher m = p.matcher(password);
-     
-    		return m.matches();
-    }
+	public static void main(String []args){
+        
+        final int MAX=8;
+                     
     
-    public static void main(String[] args) {
+        final int MIN_Uppercase=2;
+        final int MIN_Lowercase=2;
+        final int NUM_Digits=2;
+        final int Special=2;
+        int uppercaseCounter=0;
+        int lowercaseCounter=0;
+        int digitCounter=0;
+        int specialCounter=0;
+        
+       
+JOptionPane.showMessageDialog(null, "ENtre com a senha");       
+String password = JOptionPane.showInputDialog("Insira sua senha");
 
-        Scanner scanner = new Scanner(System.in);
-
-        String nome = JOptionPane.showInputDialog("nome");
-        String senha = JOptionPane.showInputDialog("senha");
-        int i = 0;
-
-        JOptionPane.showMessageDialog(null, validaSenha(senha));
-    }
+       
+        
+        for (int i=0; i < password.length(); i++ ) {
+               char c = password.charAt(i);
+               if(Character.isUpperCase(c)) 
+                     uppercaseCounter++;
+               else if(Character.isLowerCase(c)) 
+                     lowercaseCounter++;
+               else if(Character.isDigit(c)) 
+                     digitCounter++;     
+                if(c>=33&&c<=46||c==64){
+                 specialCounter++;
+             }
+               
+        }
+        
+        if (password.length() >= MAX && uppercaseCounter >= MIN_Uppercase 
+&& lowercaseCounter >= MIN_Lowercase && digitCounter >= NUM_Digits && specialCounter >= Special) { 
+        	JOptionPane.showMessageDialog(null, "Senha validada!");
+        }
+        else {
+        	JOptionPane.showMessageDialog(null, "Sua senha não contém o seguinte:");
+               if(password.length() < MAX)
+            	   JOptionPane.showMessageDialog(null, "pelo menos 8 caracteres ");
+               if (lowercaseCounter < MIN_Lowercase) 
+            	   JOptionPane.showMessageDialog(null, "Mínimo de letras minúsculas");
+               if (uppercaseCounter < MIN_Uppercase) 
+            	   JOptionPane.showMessageDialog(null, "Mínimo de letras maiúsculas");
+               if(digitCounter < NUM_Digits) 
+            	   JOptionPane.showMessageDialog(null, "Número mínimo de dígitos numéricos");
+               if(specialCounter < Special)
+            	   JOptionPane.showMessageDialog(null, "A senha deve conter pelo menos 3 caracteres especiais");
+               
+        }
+        
+   
+}
 }
